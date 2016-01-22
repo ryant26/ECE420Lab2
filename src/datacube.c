89e8e6a9c5daae2750762830405f4d52760fda83
Ryan Thornhill <ryant26@gmail.com>
2016-01-22 09:08:39 -0700

    turned data cube into 2D

--- src/datacube.c
+++ src/datacube.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 //Globals for use
-int ***cube;
+int **cube;
 int cubeSize;
 
 void create_data_cube(int size){
@@ -9,30 +9,23 @@ void create_data_cube(int size){
 	cube = malloc(size * sizeof(int *));
 	
 	int i;
-	int j;
 	for (i = 0; i < size; i++){
-		cube[i] = malloc (size * sizeof(int *));		
-		for (j = 0; j < size; j++){
-			cube[i][j] = calloc (size, sizeof(int));
-		}
+		cube[i] = malloc (size * sizeof(int));		
 	}
 }
 
-int get_value(int i, int j, int k){
-	return cube[i][j][k];
+int get_value(int i, int j){
+	return cube[i][j];
 }
 
-void set_value(int i, int j, int k, int val){
-	cube[i][j][k] = val;
+void set_value(int i, int j, int k){
+	cube[i][j] = k;
 }
 
 void destroy_data_cube(){
 	int i;
 	int j;
 	for (i = 0; i < cubeSize; i++){
-		for (j = 0; j < cubeSize; j++){
-			free(cube[i][j]);
-		}
 		free(cube[i]);
 	}
 	free(cube);
